790baafd59f4a57d8689cafeb139a49217256639
Amir Hilal <amirhilal@users.noreply.github.com>
2017-12-09 22:05:21 +0300

    Added image save into folders

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
+
 from bs4 import BeautifulSoup
 import requests
 import re
+import urllib.request
 
 # PART 1
 url = requests.get("https://www.drivearabia.com/carprices/ksa/")
@@ -30,7 +33,31 @@ while (i < len(brands)):
     carData = carUrl.text
     carSoup = BeautifulSoup(carData, 'html.parser')
     for link in carSoup.select("h2.featured-title a"):
-        print(str.lstrip(str.rstrip(re.sub(" +", ' ', link.text))))
         name = str.lstrip(str.rstrip(re.sub(" +", ' ', link.text)))
-        names.append(name)
+        nameUrl = name.replace(' ', '-')
+        print(name)
+        imageUrl = requests.get("http://www.cars-data.com/en/"+str(brands[i-1].lower())+"/"+nameUrl.replace(str(brands[i-1]+"-"), "").lower())
+        imageData = imageUrl.text
+        imageSoup = BeautifulSoup(imageData, 'html.parser')
+
+        for images in imageSoup.find_all("a", attrs={'title': name}, limit=1):
+            id = images['href'].split('/')[5]
+            folderName = nameUrl.lower()
+            try:
+                os.makedirs(folderName)
+            except FileExistsError:
+                print("Not created, already exists")
+
+            for g in range (1, 5):
+                fileName = str(nameUrl.lower())
+                imageFileName = "http://www.cars-data.com/pictures/"+ str(brands[i-1].lower()) + "/"+nameUrl.lower()+"_"+id+"_"+str(g)+".jpg"
+                imageFile = open(folderName+"/" + fileName+"_"+str(g)+".jpg", "wb")
+                print(imageFileName)
+                try:
+                    imageFile.write(urllib.request.urlopen(imageFileName).read())
+                    imageFile.close()
+                    print(str(fileName)+ " Saved")
+                except urllib.request.HTTPError:
+                    print ('Image not found')
+
     i += 1
